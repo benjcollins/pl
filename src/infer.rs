13a98c948f54862a9bca195b2bea95ba4a8099fb
Ben Collins <benjcollins18@gmail.com>
2022-04-01 17:09:09 +0100

    cleaner infer

--- src/infer.rs
+++ src/infer.rs
@@ -1,12 +1,18 @@
 use std::{rc::Rc, cell::RefCell};
 
-#[derive(Debug, Clone)]
-pub struct InferTyRef<T: Unify + Clone>(Rc<RefCell<InferTy<T>>>);
+#[derive(Debug)]
+pub struct InferTyRef<T: Unify>(Rc<RefCell<InferTy<T>>>);
 
-#[derive(Debug, Clone)]
-pub enum InferTy<T: Unify + Clone> {
+#[derive(Debug)]
+pub enum InferTy<T: Unify> {
     Equal(InferTyRef<T>),
-    Known(T),
+    Known(Option<T>),
+}
+
+impl<T: Unify> Clone for InferTyRef<T> {
+    fn clone(&self) -> Self {
+        Self(Rc::clone(&self.0))
+    }
 }
 
 pub trait Unify where Self: Sized {
@@ -16,45 +22,19 @@ pub trait Unify where Self: Sized {
     fn concrete(&self) -> Self::Concrete;
 }
 
-impl<T: Unify<Concrete = C> + Clone, C> InferTyRef<T> {
+impl<T: Unify<Concrete = C>, C> InferTyRef<T> {
     pub fn new(ty: T) -> InferTyRef<T> {
-        InferTyRef(Rc::new(RefCell::new(InferTy::Known(ty))))
+        InferTyRef(Rc::new(RefCell::new(InferTy::Known(Some(ty)))))
     }
     pub fn concrete(&self) -> C {
         match &*self.0.borrow() {
             InferTy::Equal(r) => r.concrete(),
-            InferTy::Known(ty) => ty.concrete(),
+            InferTy::Known(ty) => ty.as_ref().unwrap().concrete(),
         }
     }
 }
 
-// pub fn unify<T: Unify + Clone>(a: &InferTyRef<T>, b: &InferTyRef<T>) -> Result<InferTyRef<T>, ()> {
-//     let a_ty = std::mem::replace(&mut *a.0.borrow_mut(), InferTy::Empty);
-//     let b_ty = std::mem::replace(&mut *b.0.borrow_mut(), InferTy::Empty);
-
-//     let ty = match (a_ty, b_ty) {
-//         (InferTy::Equal(a), b_ty) => {
-//             *b.0.borrow_mut() = b_ty;
-//             unify(&a, b)?
-//         },
-//         (a_ty, InferTy::Equal(b)) => {
-//             *a.0.borrow_mut() = a_ty;
-//             unify(a, &b)?
-//         },
-        
-//         (InferTy::Known(a), InferTy::Known(b)) => {
-//             InferTyRef::new(T::unify(a, b)?)
-//         }
-
-//         _ => unreachable!(),
-//     };
-
-//     *a.0.borrow_mut() = InferTy::Equal(ty.clone());
-//     *b.0.borrow_mut() = InferTy::Equal(ty.clone());
-//     Ok(ty)
-// }
-
-pub fn unify<T: Unify + Clone>(a: &InferTyRef<T>, b: &InferTyRef<T>) -> Result<InferTyRef<T>, ()> {
+pub fn unify<T: Unify>(a: &InferTyRef<T>, b: &InferTyRef<T>) -> Result<InferTyRef<T>, ()> {
     if Rc::ptr_eq(&a.0, &b.0) {
         return Ok(a.clone())
     }
@@ -76,7 +56,7 @@ pub fn unify<T: Unify + Clone>(a: &InferTyRef<T>, b: &InferTyRef<T>) -> Result<I
             Ok(unified)
         }
         (InferTy::Known(a), InferTy::Known(b)) => {
-            let unified = InferTyRef::new(T::unify(a.clone(), b.clone())?);
+            let unified = InferTyRef::new(T::unify(a.take().unwrap(), b.take().unwrap())?);
             *a_ref = InferTy::Equal(unified.clone());
             *b_ref = InferTy::Equal(unified.clone());
             Ok(unified)
